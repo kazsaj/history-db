99f4f8518922a79ae88174fd2417b9b246fb2996
kazsaj <ksajkowski@gmail.com>
2015-09-06 18:53:43 +0100

    schema updated: - using a hash for primary key instead of AI id, to make use of the INSERT INTO ON DUPLICATE KEY UPDATE sql syntax - added timestamp index - added username - more appropriate field type for count field - not declaring charset, nor AI value

--- db/schema.sql
+++ db/schema.sql
@@ -1,11 +1,13 @@
 
-CREATE DATABASE `history` /*!40100 DEFAULT CHARACTER SET latin1 */
+CREATE DATABASE `history`;
 
-CREATE TABLE `history` (
-     `id` int(11) NOT NULL AUTO_INCREMENT,
-     `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-     `command` text,
-     `hostname` text,
-     `count` int(11) DEFAULT NULL,
-     PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=11521 DEFAULT CHARSET=latin1
+CREATE TABLE `history`.`history` (
+	`history_hash` VARCHAR(40) NOT NULL,
+	`username` VARCHAR(64) NOT NULL,
+	`hostname` VARCHAR(64) NOT NULL,
+	`command` TEXT NOT NULL,
+	`count` INT(11) UNSIGNED NOT NULL DEFAULT '1',
+	`timestamp` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+	PRIMARY KEY (`history_hash`),
+	INDEX `timestamp` (`timestamp`)
+) ENGINE=InnoDB;
